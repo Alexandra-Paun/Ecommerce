44e848572b65bb6b11a67a20860629bfd8bdda3c
Alexandra Paun <alexandrapaun96@yahoo.com>
2023-07-01 17:07:55 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      app
+    </div>
+  );
+}
+
+export default App;
